2dcbbb754f29072fbfd74d2d77697fcca5e25c3c
Zach Cullen <zacullen@gmail.com>
2020-01-09 19:51:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
 
@@ -8,3 +9,7 @@ doc = Nokogiri::HTML(html)
 courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .title-oE5vT4").collect {|child| child.text}
 
 courses.each {|course| puts course}
+
+binding.pry
+
+puts "goodbye"
